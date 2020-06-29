236f03fa63807992bbdaa4fcdd1750cd5a2e2d39
Kishore-kalastri <62170733+Kishore-kalastri@users.noreply.github.com>
2020-06-29 10:48:18 +0530

    Update server.js

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const HOST = '0.0.0.0';
 // App
 const app = express();
 app.get('/', (req, res) => {
-  res.send('Hello World !! GOOD NIGHT SD');
+  res.send('Hello World !! GOOD Morning');
 });
 
 app.listen(PORT, HOST);
