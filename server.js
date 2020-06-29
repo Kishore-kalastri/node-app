32518cf7d1ea46ed0b667e1858971b633e46e129
Kishore-kalastri <62170733+Kishore-kalastri@users.noreply.github.com>
2020-06-29 20:18:08 +0530

    Update server.js

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const HOST = '0.0.0.0';
 // App
 const app = express();
 app.get('/', (req, res) => {
-  res.send('Hello World !! Kishore');
+  res.send('Hello World !! NIGHT');
 });
 
 app.listen(PORT, HOST);
